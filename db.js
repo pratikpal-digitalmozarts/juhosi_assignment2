bf3e7aa5451a383cd9f077ae565d6351f1707af3
pratikpal-digitalmozarts <97144487+pratikpal-digitalmozarts@users.noreply.github.com>
2023-06-28 22:16:47 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 
-const mysql = require('mysql2')
+const mysql = require('mysql')
 const pool = mysql.createPool({
     host: "sql12.freemysqlhosting.net",
     user: "sql12629011",
